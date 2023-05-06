50f847198418d0bef508e8a75bc93980ea7373fc
Sam Lantinga <slouken@libsdl.org>
2023-05-06 08:44:19 -0700

    Removed redundant __powerpc__ check
    (cherry picked from commit https://github.com/libsdl-org/SDL/commit/b6df25c33497388f16a4ed2af896ef14277c8865)

--- src/video/SDL_blit_N.c
+++ src/video/SDL_blit_N.c
@@ -162,7 +162,7 @@ static vector unsigned char calc_swizzle32(const SDL_PixelFormat *srcfmt,
     return(vswiz);
 }
 
-#if defined(__powerpc__) && (SDL_BYTEORDER == SDL_LIL_ENDIAN)
+#if SDL_BYTEORDER == SDL_LIL_ENDIAN
 /* reorder bytes for PowerPC little endian */
 static vector unsigned char reorder_ppc64le_vec(vector unsigned char vpermute)
 {
@@ -625,7 +625,7 @@ static void Blit32to32KeyAltivec(SDL_BlitInfo *info)
     ((unsigned int *)(char*)&vrgbmask)[0] = rgbmask;
     vrgbmask = vec_splat(vrgbmask, 0);
 
-#if defined(__powerpc__) && (SDL_BYTEORDER == SDL_LIL_ENDIAN)
+#if SDL_BYTEORDER == SDL_LIL_ENDIAN
     /* reorder bytes for PowerPC little endian */
     vpermute = reorder_ppc64le_vec(vpermute);
 #endif
@@ -724,7 +724,7 @@ static void ConvertAltivec32to32_noprefetch(SDL_BlitInfo *info)
     assert(srcfmt->BytesPerPixel == 4);
     assert(dstfmt->BytesPerPixel == 4);
 
-#if defined(__powerpc__) && (SDL_BYTEORDER == SDL_LIL_ENDIAN)
+#if SDL_BYTEORDER == SDL_LIL_ENDIAN
     /* reorder bytes for PowerPC little endian */
     vpermute = reorder_ppc64le_vec(vpermute);
 #endif
@@ -811,7 +811,7 @@ static void ConvertAltivec32to32_prefetch(SDL_BlitInfo *info)
     assert(srcfmt->BytesPerPixel == 4);
     assert(dstfmt->BytesPerPixel == 4);
 
-#if defined(__powerpc__) && (SDL_BYTEORDER == SDL_LIL_ENDIAN)
+#if SDL_BYTEORDER == SDL_LIL_ENDIAN
     /* reorder bytes for PowerPC little endian */
     vpermute = reorder_ppc64le_vec(vpermute);
 #endif
