9cfbedc8107d63cfb14bcf852323211ed4e325f4
Miro Kropacek <miro.kropacek@gmail.com>
2024-06-28 22:50:14 +0200

    atari:video:gem: Fix commit ea0674c9
    Perhaps overlooked.

--- src/video/gem/SDL_gemevents.c
+++ src/video/gem/SDL_gemevents.c
@@ -148,7 +148,7 @@ void GEM_PumpEvents(_THIS)
 
 	/* Update mouse state */
 	graf_mkstate(&mousex, &mousey, &mouseb, &em_out.emo_kmeta);
-	em_out.emo_kmeta |= (Kbshift(-1) & K_CAPSLOCK);	/* MU_KEYBD is not aware of CAPSLOCK */
+	em_out.emo_kmeta |= (Kbshift(-1) & (0x80 | K_CAPSLOCK));	/* MU_KEYBD is not aware of AltGr and CAPSLOCK */
 	do_keyboard_special(em_out.emo_kmeta, cur_tick);
 	do_mouse_motion(this, mousex, mousey);
 	do_mouse_buttons(this, mouseb);
@@ -390,8 +390,7 @@ static void do_mouse_motion(_THIS, short mx, short my)
 
 static int atari_GetButton(int button)
 {
-	switch(button)
-	{
+	switch(button) {
 		case 0:
 		default:
 			return SDL_BUTTON_LEFT;
